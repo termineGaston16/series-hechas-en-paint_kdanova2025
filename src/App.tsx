184b7fec4d9beace0e548eb5975eabde9b9ccf1b
kda/nova <gastontermine16@gmail.com>
2025-03-17 19:21:01 -0300

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element='ERROR 404' />
+            </Routes>
+        </BrowserRouter>
+    )
+}
