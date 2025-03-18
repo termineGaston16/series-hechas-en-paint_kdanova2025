95ca862e8a7f5dfe8da1f3a542fabd0acb0826b7
kda/nova <gastontermine16@gmail.com>
2025-03-18 16:35:16 -0300

    dejar un archivo como base MOCKS

--- src/GENRES/Infraestructure/genresAPI.ts
+++ src/GENRES/Infraestructure/genresAPI.ts
@@ -5,16 +5,6 @@ import { db } from "../../UI/FIREBASE/database";
 export const MAX_LENGTH_GENRES = 1;
 
 export const getGenres = async (lastID: string | null): Promise<Gender_I[]> => {
-    const genresRef = collection(db, "GENRES");
-    let q;
 
-    if (lastID === null) {
-        q = query(genresRef, orderBy("id", 'asc'), limit(MAX_LENGTH_GENRES));
-    } else {
-        q = query(genresRef)
-    }
-
-    const querySnapshot = await getDocs(q);
-    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as Gender_I[];
 };
 
