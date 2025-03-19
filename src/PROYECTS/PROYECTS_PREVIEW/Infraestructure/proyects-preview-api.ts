08f39533fba8901b592de317304a2a6c3192fd57
kda/nova <gastontermine16@gmail.com>
2025-03-19 11:42:11 -0300

    funcion getProjectsPreviewByCategory creada correctamente!

--- src/PROYECTS/PROYECTS_PREVIEW/Infraestructure/proyects-preview-api.ts
+++ src/PROYECTS/PROYECTS_PREVIEW/Infraestructure/proyects-preview-api.ts
@@ -5,6 +5,7 @@ import { ProyectPreview_I } from "../Domain/proyect_preview";
 
 export const MAX_LENGTH_PROJECTS_PREVIEW_BY_GENDER = 5;
 export const MAX_LENGTH_GET_PROYECTS_PREVIEW_KEYSEARCH = 5;
+export const MAX_LENGTH_GET_PROYECTS_PREVIEW_CATEGORY = 5;
 
 export const getProjectsPreviewByGender = async (
     keyName: string | null,
@@ -108,3 +109,54 @@ export const getProjectsPreviewByKeySearch = async (
     }
 
 }
+
+export const getProjectsPreviewByCategory = async (
+    keyName: string | null,
+    category: string
+): Promise<ProyectPreview_I[]> => {
+
+    if (typeof keyName !== 'string'
+        && keyName !== null
+        && typeof category !== 'string'
+        || category.length <= 0
+    ) throw new TypeError('The received arguments are invalid.')
+
+    try {
+        if (!db) throw new DataBaseError('The database is not initialized.');
+
+        const proyectsRef = collection(db, 'PROYECTS');
+        let q;
+
+        if (keyName === null) q = query(
+            proyectsRef,
+            where('category', '==', category),
+            orderBy('keyName', "asc"),
+            limit(MAX_LENGTH_GET_PROYECTS_PREVIEW_CATEGORY)
+        )
+        if (typeof keyName === 'string') {
+            const lastProyectRef = doc(db, 'PROYECTS', keyName)
+            const lastProyectSnap = await getDoc(lastProyectRef);
+
+            if (!lastProyectSnap.exists()) return [];
+
+            q = query(
+                proyectsRef,
+                where('category', '==', category),
+                orderBy('keyName', "asc"),
+                startAfter(lastProyectSnap),
+                limit(MAX_LENGTH_GET_PROYECTS_PREVIEW_CATEGORY)
+            );
+        }
+        if (!q) return [];
+
+        const querySnapshot = await getDocs(q);
+        return querySnapshot.docs.map(doc => ({
+            key_name: doc.id, ...doc.data()
+        })) as ProyectPreview_I[];
+
+
+    } catch (error) {
+        if (error instanceof DataBaseError) throw error;
+        throw new DataBaseSystemFailure(`Firestore query failed: ${error}`);
+    }
+};
